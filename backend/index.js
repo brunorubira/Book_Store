f55ab061bdc9bfb765b4d03f4d21731276c26225
brunorubira <brunorcorrea@hotmail.com>
2024-01-31 12:41:37 -0300

    react_vite

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,19 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose'
 import { Book } from './models/bookModels.js';
 import booksRoutes from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json())
 
 app.use('/books', booksRoutes)
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.get('/', (req, res) => {
     console.log(req)
